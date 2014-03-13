1548e2f0d05c9ff54d4287c16ef34f8933053198
michael <zhuxiaohui001@163.com>
2014-03-13 15:03:55 +0800

    1，add arcview

--- src/mobile/android/ch15/draw/ArcView.java
+++ src/mobile/android/ch15/draw/ArcView.java
@@ -11,31 +11,24 @@ import android.view.View;
  * Created by michael on 14-3-13.
  */
 class ArcView extends View {
-    private Paint paint1 = new Paint();
-    private Paint paint2 = new Paint();
-    private Paint paint3 = new Paint();
+    private Paint paint = new Paint();
+
     private boolean useCenter = true;
     public ArcView(Context context) {
         super(context);
         setBackgroundColor(Color.WHITE);
 
-        paint1.setColor(Color.BLACK);
-        paint1.setStrokeWidth(2);
-        paint2.setColor(Color.RED);
-        paint2.setStrokeWidth(4);
-        paint3.setColor(Color.BLUE);
-        paint3.setStrokeWidth(8);
+        paint.setColor(Color.GREEN);
+        paint.setStrokeWidth(6);
 
     }
-
-
     @Override
     protected void onDraw(Canvas canvas) {
 
-        paint2.setStyle(Paint.Style.STROKE);
-        canvas.drawCircle(360, 110, 40, paint2);
-        paint2.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(360, 110, 30, paint2);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(360, 110, 40, paint);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(360, 110, 30, paint);
 
         RectF rectF1 = new RectF();
         rectF1.left = 30;
@@ -43,24 +36,26 @@ class ArcView extends View {
         rectF1.right = 120;
         rectF1.bottom = 280;
 
-        canvas.drawArc(rectF1, 30, 100, useCenter, paint2);
+        canvas.drawArc(rectF1, 30, 60, useCenter, paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawArc(rectF1, 90, 20, useCenter, paint);
 
-        RectF rectF2 = new RectF();
-        rectF2.left = 140;
-        rectF2.top = 190;
-        rectF2.right = 280;
-        rectF2.bottom = 290;
-        paint2.setStyle(Paint.Style.STROKE);
-        canvas.drawArc(rectF2, 0, 360, useCenter, paint2);
+//        RectF rectF2 = new RectF();
+//        rectF2.left = 140;
+//        rectF2.top = 190;
+//        rectF2.right = 280;
+//        rectF2.bottom = 290;
+//        paint.setStyle(Paint.Style.STROKE);
+//        canvas.drawArc(rectF2, 0, 360, useCenter, paint);
 
-        RectF rectF3 = new RectF();
-        rectF3.left = 160;
-        rectF3.top = 190;
-        rectF3.right = 260;
-        rectF3.bottom = 290;
-
-        paint3.setStyle(Paint.Style.STROKE);
-        canvas.drawArc(rectF3, 0, 360, useCenter, paint3);
+//        RectF rectF3 = new RectF();
+//        rectF3.left = 160;
+//        rectF3.top = 190;
+//        rectF3.right = 260;
+//        rectF3.bottom = 290;
+//
+//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+//        canvas.drawArc(rectF3, 0, 360, useCenter, paint);
 
     }
 }
