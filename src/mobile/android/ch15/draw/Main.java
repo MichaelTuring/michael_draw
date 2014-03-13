a85a26c9719c132e3c69ddd206c5dfc659ad9d4b
michael <zhuxiaohui001@163.com>
2014-03-13 14:10:23 +0800

    test

--- src/mobile/android/ch15/draw/Main.java
+++ src/mobile/android/ch15/draw/Main.java
@@ -83,7 +83,6 @@ public class Main extends Activity {
                 canvas.drawLine(pts[i], pts[i + 1], pts[stopXIndex],
                         pts[stopYIndex], paint);
             }
-
         }
 
         @Override
